fc579b621c8a827cc9ad206cafb4827f4080eb90
labmouse41 <107990826+labmouse41@users.noreply.github.com>
2023-04-07 11:15:42 +0900

    shuffle system with 1 temp

--- main.cpp
+++ main.cpp
@@ -14,15 +14,28 @@ int main()
 		ballNumber[i] = i+1;
 	}
 
-	for (int i = 0; i < 6; i++)
+	srand((unsigned int)time(0));
+
+	for (int i = 0; i < 999; i++)
 	{
-		randomNumber = rand() % 45;
-		if (ballNumber[randomNumber] != 0)
-		{
-			out[i] = ballNumber[randomNumber];
-			ballNumber[randomNumber] = 0;
-			cout << out[i] << endl;
-		}
+		int First = rand()%45;
+		int Second = rand()%45;
+		
+		int Temp = ballNumber[First];
+		ballNumber[First] = ballNumber[Second];
+		ballNumber[Second] = Temp;
+	}
+	
+	for (int i = 0; i < 45; i++)
+	{
+		cout << ballNumber[i] << endl;
+		//randomNumber = rand() % 45;
+		//if (ballNumber[randomNumber] != 0)
+		//{
+		//	out[i] = ballNumber[randomNumber];
+		//	ballNumber[randomNumber] = 0;
+		//	cout << out[i] << endl;
+		//}
 	}
 
 	return 0;
